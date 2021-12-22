69c892b14548733a903e3104f827321b314e7806
PeroHub <peterime600@gmail.com>
2021-12-22 09:09:02 +0100

    changes

--- src/index.js
+++ src/index.js
@@ -1,28 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Components
 
-import Header from './Components/header';
-import ProductDescription from './Components/product_description';
-import ProductList from './Components/product_list';
+// import Header from './Components/Header';
+// import ProductDescription from './Components/product_description';
+// import ProductList from './Components/Product_list';
 
-const App = () => {
-  return(
-    <div>
-      <Router>
-      <Header/>
+// import { Products } from './Components/arrayofitems'
+import App from './App'
 
-        <Routes>
-          <Route path="/" element={<ProductList/>} />
-          <Route path="/product_description" element={<ProductDescription/>} />
-        </Routes>
-      </Router>
+// const App = () => {
+
+//   let items = Products()
+    
+//     console.log(items)
+//   return(
+//     <div>
+//       <Router>
+//       <Header/>
+
+//         <Routes>
+//           <Route path="/" element={<ProductList items={items}/>} />
+//           <Route path="/product_description" element={<ProductDescription/>} />
+//         </Routes>
+//       </Router>
       
       
-    </div>
-  )
-}
+//     </div>
+//   )
+// }
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
